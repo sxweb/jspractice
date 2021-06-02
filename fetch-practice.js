cdec399d97b728a47686aceb2fb2c2b5fa102f3f
sxweb <phpred@yandex.ru>
2021-06-02 09:17:03 +0300

    GotObject for ice and fire api

--- fetch-practice.js
+++ fetch-practice.js
@@ -1,4 +1,29 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(console.log('some error'));
+class GotObjects{
+  
+  constructor(){
+    this.baseUrl = 'https://anapioficeandfire.com/api/'
+  }
+  async getObject(objType, id){
+    let type = '';
+    switch(objType){
+      case 'character':
+        type = 'characters';
+        break;
+      case 'house':
+        type = 'houses';
+        break;
+      case 'book':
+        type = 'books';
+        break;
+    }
+    const res = await fetch(`${this.baseUrl}${type}/${id}`);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}. Status: ${res.status}`)
+    }
+    return await res.json();
+  }
+}
+
+const characters = new GotObjects();
+characters.getObject('book', 1)
+.then(character => console.log(character));
